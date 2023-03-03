b382f8acf5c6e1d2f96cac112324e3b09cf42c74
Rosalie Wagner <mail@rosaliewagner.com>
2023-03-03 17:28:56 +0100

    Separated sources Roman/Italic and moved m-default master

--- Sources/build.sh
+++ Sources/build.sh
@@ -5,15 +5,13 @@ set -e
 echo "Build with gftools builder"
 gftools builder config.yaml
 
-echo "Slice into Roman and Italic VF (as Google Fonts currently doesn't support slnt or ital axes)"
+echo "Move default instance to regular"
+fonttools varLib.instancer "../fonts/variable/Anybody[wdth,wght].ttf" wdth=25:100:150 --update-name-table -o "../fonts/variable/Anybody[wdth,wght].ttf"
 
-fonttools varLib.instancer "../fonts/variable/Anybody[slnt,wdth,wght].ttf" slnt=0 --update-name-table -o "../fonts/variable/Anybody[wdth,wght].ttf"
-fonttools varLib.instancer "../fonts/variable/Anybody[slnt,wdth,wght].ttf" slnt=-10 --update-name-table -o "../fonts/variable/Anybody-Italic[wdth,wght].ttf"
+echo "Move default instance to regular"
+fonttools varLib.instancer "../fonts/variable/Anybody-Italic[wdth,wght].ttf" wdth=25:100:150 --update-name-table -o "../fonts/variable/Anybody-Italic[wdth,wght].ttf"
 
-echo "Fix name ID 25"
-gftools gen-stat "../fonts/variable/Anybody-Italic[wdth,wght].ttf" --axis-order 'wdth' 'wght' 'ital' --inplace
-
-echo "Fix style linking"
+echo "Fix nameID 6 (fonttools whatever bug)"
 python3 fix.py
 
 echo "Complete"
--- Sources/fix.py
+++ Sources/fix.py
@@ -9,20 +9,8 @@ if __name__ == "__main__":
 
     for font_path in glob.glob("../fonts/variable/*Italic*.ttf"):
         with open(font_path, "rb") as f:
-            print("Fix italic bits in {}".format(font_path))
+            print("Fix italic name id 6 in {}".format(font_path))
             ttFont = TTFont(f)
-            ttFont["head"].macStyle = MacStyle.ITALIC
-            ttFont["OS/2"].fsSelection = FsSelection.USETYPOMETRICS | FsSelection.ITALIC
-            ttFont["post"].italicAngle = -10
-            ttFont["hhea"].caretSlopeRise = 1000
-            ttFont["hhea"].caretSlopeRun = 176
-            ttFont.save(font_path)
-    
-    for font_path in glob.glob("../fonts/ttf/*Italic*.ttf"):
-        with open(font_path, "rb") as f:
-            print("Fix italic bits in {}".format(font_path))
-            ttFont = TTFont(f)
-            ttFont["post"].italicAngle = -10
-            ttFont["hhea"].caretSlopeRise = 1000
-            ttFont["hhea"].caretSlopeRun = 176
+            ttFont["name"].setName("Anybody-ThinItalic", 6, 3, 1, 0x409)
+            ttFont["name"].setName("1.113;ETCO;Anybody-ThinItalic", 3, 3, 1, 0x409)
             ttFont.save(font_path)
